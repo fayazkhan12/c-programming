866e62381a47f2abe30ebcbe2e01f246786e5c01
fayazkhan12 <83631202+fayazkhan12@users.noreply.github.com>
2021-05-19 23:46:30 +0530

    Add files via upload

---
+++ assignment.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	
+	int a=12;
+	int b=23;
+	printf("print if a is greagter than b: %d\n",a>b,a,b);
+	printf("printf if b is grater than a: %d\n",b>a,a,b);
+	printf("print if a is equal to b: %d\n",a==b,a,b);
+	getch();
+}
