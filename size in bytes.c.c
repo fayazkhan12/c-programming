1bd5d84f69805bd40206c6c54fc38c1a63e79e25
fayazkhan12 <83631202+fayazkhan12@users.noreply.github.com>
2021-05-19 23:45:41 +0530

    Add files via upload

---
+++ size in bytes.c.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	float b;
+	double c;
+	char d ;
+printf("size of char in bytes %ld\n",sizeof(d));	
+printf("size of int in bytes %ld\n",sizeof(a));
+printf("size of float in bytes %d\n",sizeof(b));
+printf("size of double in bytes %d\n",sizeof(c));
+getch();
+}
