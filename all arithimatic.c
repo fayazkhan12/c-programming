7c36e94419bf2a7f9f0c56c6a039a60e03b9efb1
fayazkhan12 <83631202+fayazkhan12@users.noreply.github.com>
2021-07-16 22:43:41 +0530

    Create all arithimatic.c

---
+++ all arithimatic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	a=12;
+	b=23;
+	printf("sum of two numbers is: %d\n",a+b,a,b);
+		printf("subtraction of two numbers is: %d\n",a-b,a,b);
+			printf("  multiplication of two numbers is: %d\n",a*b,a,b);
+				printf(" division of two numbers is: %d\n",a/b,a,b);
+					printf("reminder of two numbers is: %d\n",a%b,a,b);
+		
+}
