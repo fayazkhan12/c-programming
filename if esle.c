588ef4231a60f19da1c2678df78e6184ec793685
fayazkhan12 <83631202+fayazkhan12@users.noreply.github.com>
2021-07-16 22:41:51 +0530

    Create if esle.c

---
+++ if esle.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+void main()
+{
+	
+	int a=12;
+	int b=34;
+	int c=23;
+	
+	if (a>b && a>c);
+	{
+		printf("a is greater");
+	}else if (b>a && b>c);
+	{
+		printf("b is greater");
+		
+	}else{
+		printf("c is greater");
+	}
+	getch();
+}
